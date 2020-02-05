fbb55033d7cd5cf28020087e08019b4cfdf6684c
csf16cav <Liverpool6>
2020-02-05 16:16:57 +0000

    Firebase setup

--- app/src/main/java/com/example/midulsterfootballleague/Register.java
+++ app/src/main/java/com/example/midulsterfootballleague/Register.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     public void register (View view){
