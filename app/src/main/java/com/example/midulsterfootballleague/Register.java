24992c3beb817319455c47b6c77eed3fb7bd04b9
davidcairns35 <davidcairns35@gmail.com>
2020-02-05 23:20:18 +0000

    Registration connected to firebase for email and password

--- app/src/main/java/com/example/midulsterfootballleague/Register.java
+++ app/src/main/java/com/example/midulsterfootballleague/Register.java
@@ -1,17 +1,25 @@
 package com.example.midulsterfootballleague;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.ProgressBar;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+
 
 public class Register extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
-    private ProgressBar progressBar;
+    //private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,37 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void registerUser (View view){
+        //getting the email and password
+        String email = ((EditText) findViewById(R.id.email)).getText().toString();
+        String password = ((EditText) findViewById(R.id.password)).getText().toString();
+        //checking email and password are empty
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_LONG).show();
+            return;
+        }
+        //creating a new user
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if (task.isSuccessful()){
+                            Toast.makeText(Register.this, "Registered succesfully", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(Register.this, MainActivity.class));
+                            finish();
+                        } else {
+                            FirebaseAuthException e = (FirebaseAuthException) task.getException();
+                            Toast.makeText(Register.this, "Registration failed: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
     public void register (View view){
