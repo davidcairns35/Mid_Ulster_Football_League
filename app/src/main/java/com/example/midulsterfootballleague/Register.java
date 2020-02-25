64102062933bd8fc23a9f31c371ffa3e9d701bc5
davidcairns35 <davidcairns35@gmail.com>
2020-02-25 23:05:07 +0000

    Realtime database set up for registration page 2

--- app/src/main/java/com/example/midulsterfootballleague/Register.java
+++ app/src/main/java/com/example/midulsterfootballleague/Register.java
@@ -2,19 +2,29 @@ package com.example.midulsterfootballleague;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class Register extends AppCompatActivity {
-    private DatabaseReference mDatabase;
+    EditText email;
+    EditText username;
+    EditText password;
+    EditText confirmPassword;
+    Button register;
+
+    FirebaseAuth firebaseAuth;
+    //private DatabaseReference mDatabase;
     //private ProgressBar progressBar;
 
     @Override
@@ -22,17 +32,42 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference("user");
+        email = findViewById(R.id.email);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirmPassword);
+        register = findViewById(R.id.register);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(Register.this, "Registered Successfully", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(Register.this, MainActivity.class);
+                                    startActivity(intent);
+                                }else{
+                                    Toast.makeText(Register.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 
-    public void registerUser (View view){
+    /*public void registerUser (View view){
         //getting the email and password
         String email = ((EditText) findViewById(R.id.email)).getText().toString();
         String username = ((EditText) findViewById(R.id.username)).getText().toString();
         String password = ((EditText) findViewById(R.id.password)).getText().toString();
         String confirmPassword = ((EditText) findViewById(R.id.confirmPassword)).getText().toString();
         //checking email and password are empty
-        if(TextUtils.isEmpty(email)){
+        /*if(TextUtils.isEmpty(email)){
             Toast.makeText(this, "Please enter email", Toast.LENGTH_LONG).show();
             return;
         }
@@ -47,16 +82,14 @@ public class Register extends AppCompatActivity {
         if(TextUtils.isEmpty(confirmPassword)){
             Toast.makeText(this, "The two passwords don't match", Toast.LENGTH_LONG).show();
             return;
-        }
+        }*/
         //creating a new user
-        mDatabase.child("email").setValue(email);
-        mDatabase.child("username").setValue(username);
-        mDatabase.child("password").setValue(password);
-        mDatabase.child("confirm password").setValue(confirmPassword);
-    }
+        //mDatabase.child("email").setValue(email);
+        //mDatabase.child("username").setValue(username);
+        //mDatabase.child("password").setValue(password);
+        //mDatabase.child("confirm password").setValue(confirmPassword);
 
-    public void register (View view){
-        Intent intent = new Intent(Register.this, MainActivity.class);
+        /*Intent intent = new Intent(Register.this, MainActivity.class);
         startActivity(intent);
-    }
+    }*/
 }
