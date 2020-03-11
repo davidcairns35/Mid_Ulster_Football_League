3170b4cb0a255b70d484f6a774aa257970862c74
csf16cav <csf16cav@MG122PC12>
2020-03-11 14:57:17 +0000

    Register and Login complete

--- app/src/main/java/com/example/midulsterfootballleague/Login.java
+++ app/src/main/java/com/example/midulsterfootballleague/Login.java
@@ -2,20 +2,28 @@ package com.example.midulsterfootballleague;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
     private androidx.appcompat.widget.Toolbar toolbar;
 
-    private View view;
     Button login;
-    EditText username, password;
+    EditText email, password;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,23 +41,46 @@ public class Login extends AppCompatActivity {
 
 
         login = findViewById(R.id.Login);
-        username = findViewById(R.id.Username);
+        email = findViewById(R.id.Email);
         password = findViewById(R.id.Password);
+        firebaseAuth = FirebaseAuth.getInstance();
 
-
-        login.setOnClickListener(new View.OnClickListener()
-        {
+        login.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                login(view);
+            public void onClick(View view){
+                String Email = email.getText().toString();
+                String Password = password.getText().toString();
+
+                if (TextUtils.isEmpty(Email)){
+                    email.setError("Please enter a valid email");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(Password)){
+                    password.setError("Please enter a valid password");
+                    return;
+                }
+
+                if (Password.length() < 8){
+                    password.setError("A password must contain at least 8 characters");
+                    return;
+                }
+
+                firebaseAuth.signInWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(Login.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(Login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
             }
         });
-    }
-
-    public void login (View view){
-
-
     }
 
     public void register (View view){
--- app/src/main/java/com/example/midulsterfootballleague/Register.java
+++ app/src/main/java/com/example/midulsterfootballleague/Register.java
@@ -2,6 +2,7 @@ package com.example.midulsterfootballleague;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,10 +19,10 @@ import com.google.firebase.auth.FirebaseAuth;
 
 
 public class Register extends AppCompatActivity {
-    EditText email;
+    EditText Email;
     EditText username;
-    EditText password;
-    EditText confirmPassword;
+    EditText Password;
+    EditText ConfirmPassword;
     Button register;
     private androidx.appcompat.widget.Toolbar toolbar;
 
@@ -44,10 +45,10 @@ public class Register extends AppCompatActivity {
         getSupportActionBar().setTitle("Register");
 
 
-        email = findViewById(R.id.email);
+        Email = findViewById(R.id.Email);
         username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-        confirmPassword = findViewById(R.id.confirmPassword);
+        Password = findViewById(R.id.password);
+        ConfirmPassword = findViewById(R.id.confirmPassword);
         register = findViewById(R.id.register);
 
         firebaseAuth = FirebaseAuth.getInstance();
@@ -55,19 +56,43 @@ public class Register extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()){
-                                    Toast.makeText(Register.this, "Registered Successfully", Toast.LENGTH_LONG).show();
-                                    Intent intent = new Intent(Register.this, MainActivity.class);
-                                    startActivity(intent);
-                                }else{
-                                    Toast.makeText(Register.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
+
+                String email = Email.getText().toString();
+                String password = Password.getText().toString();
+                String confirmPassword = ConfirmPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)){
+                    Email.setError("Please enter a valid email");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)){
+                    Password.setError("Please enter a valid password");
+                    return;
+                }
+
+                if (password.length() < 8){
+                    Password.setError("A password must contain at least 8 characters");
+                    return;
+                }
+
+                if (!password.equals(confirmPassword)){
+                    ConfirmPassword.setError("Passwords do not match");
+                    return;
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password) .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this, "Registered Successfully", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(Register.this, MainActivity.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(Register.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
