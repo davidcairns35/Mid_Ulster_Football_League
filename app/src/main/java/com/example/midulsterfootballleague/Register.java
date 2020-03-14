d4c7ebd3433bb54ffabbb3b9d90b50de3d379ebb
davidcairns35 <davidcairns35@gmail.com>
2020-03-14 11:26:58 +0000

    register edit to add users name complete

--- app/src/main/java/com/example/midulsterfootballleague/Register.java
+++ app/src/main/java/com/example/midulsterfootballleague/Register.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,16 +16,23 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
 
-public class Register extends AppCompatActivity implements View.OnClickListener {
+
+public class Register extends AppCompatActivity {
 
     private EditText Email, Name, Password, ConfirmPassword;
+
+    private Button register;
 
     private androidx.appcompat.widget.Toolbar toolbar;
 
     FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,86 +48,84 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
         actionBar.setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Register");
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                Intent intent = new Intent(Register.this, HomePage.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        };
 
         Email = findViewById(R.id.Email);
+        Name = findViewById(R.id.Name);
         Password = findViewById(R.id.password);
         ConfirmPassword = findViewById(R.id.confirmPassword);
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        register = findViewById(R.id.register);
 
-        findViewById(R.id.register).setOnClickListener(this);
-    }
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        private void registerUser () {
-            final String name = Name.getText().toString().trim();
-            final String email = Email.getText().toString().trim();
-            String password = Password.getText().toString().trim();
-            String confirmPassword = ConfirmPassword.getText().toString().trim();
+                String name = Name.getText().toString();
+                String email = Email.getText().toString();
+                String password = Password.getText().toString();
+                String confirmPassword = ConfirmPassword.getText().toString().trim();
 
 
-            if (TextUtils.isEmpty(email)) {
-                Email.setError("Please enter a valid email");
-                return;
-            }
-
-            if (name.isEmpty()) {
-                Name.setError("Please enter Your Full Name");
-                return;
-            }
-
-            if (TextUtils.isEmpty(password)) {
-                Password.setError("Please enter a valid password");
-                return;
-            }
-
-            if (password.length() < 8) {
-                Password.setError("A password must contain at least 8 characters");
-                return;
-            }
-
-            if (!password.equals(confirmPassword)) {
-                ConfirmPassword.setError("Passwords do not match");
-                return;
-            }
-
-            firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-
-                    if (task.isSuccessful()) {
-
-                        User user = new User(
-                                name,
-                                email
-                        );
-
-                        FirebaseDatabase.getInstance().getReference("Users")
-                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(Register.this, "Registered successfully", Toast.LENGTH_LONG).show();
-                                } else {
-                                    Toast.makeText(Register.this, "Failed to register user", Toast.LENGTH_LONG).show();
-                                }
-                            }
-                        });
-
-                    } else {
-                        Toast.makeText(Register.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                    }
+                if (TextUtils.isEmpty(email)) {
+                    Email.setError("Please enter a valid email");
+                    return;
                 }
-            });
-        }
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.register:
-                registerUser();
-                break;
-        }
-        Intent intent = new Intent(Register.this, HomePage.class);
-        startActivity(intent);
+
+                if (TextUtils.isEmpty(name)) {
+                    Name.setError("Please enter Your full name");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Password.setError("Please enter a valid password");
+                    return;
+                }
+
+                if (password.length() < 8) {
+                    Password.setError("A password must contain at least 8 characters");
+                    return;
+                }
+
+                if (!password.equals(confirmPassword)) {
+                    ConfirmPassword.setError("Passwords do not match");
+                    return;
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            String user_id = firebaseAuth.getCurrentUser().getUid();
+                            DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+
+                            String name = Name.getText().toString();
+
+                            Map newPost = new HashMap();
+                            newPost.put("name", name);
+                            current_user_db.setValue(newPost);
+
+                            Toast.makeText(Register.this, "Registered successfully", Toast.LENGTH_LONG).show();
+
+                            Intent intent = new Intent(Register.this, HomePage.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(Register.this, "Failed to register user", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
