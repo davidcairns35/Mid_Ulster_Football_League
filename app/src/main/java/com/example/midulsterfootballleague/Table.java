4b664a37af300da0141542c57a820345537abf95
davidcairns35 <davidcairns35@gmail.com>
2020-03-25 23:35:07 +0000

    division 1 table still in progress!!

--- app/src/main/java/com/example/midulsterfootballleague/Table.java
+++ app/src/main/java/com/example/midulsterfootballleague/Table.java
@@ -8,9 +8,10 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,6 +22,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.core.Context;
 
@@ -34,15 +36,17 @@ public class Table extends AppCompatActivity {
     ListView listView;
     ArrayList<League> leagues = new ArrayList<>();
     Context c;
+    FirebaseHelper helper;
+    EditText Position, Team, Played, Points, Goals;
 
     public class FirebaseHelper {
         DatabaseReference databaseReference;
         ListView listView;
         ArrayList<League> leagues = new ArrayList<>();
-        Context c;
+        Table c;
 
 
-        public FirebaseHelper(DatabaseReference databaseReference, Context context, ListView listView) {
+        public FirebaseHelper(DatabaseReference databaseReference, Table context, ListView listView) {
             this.databaseReference = databaseReference;
             this.c = context;
             this.listView = listView;
@@ -59,7 +63,7 @@ public class Table extends AppCompatActivity {
                             League league = ds.getValue(League.class);
                             leagues.add(league);
                         }
-                        adapter = new CustomAdapter(c, leagues);
+                        CustomAdapter adapter = new CustomAdapter(c, leagues);
                         listView.setAdapter(adapter);
 
                         new Handler().post(new Runnable() {
@@ -85,10 +89,10 @@ public class Table extends AppCompatActivity {
     }
 
     class CustomAdapter extends BaseAdapter {
-        Context c;
+        Table c;
         ArrayList<League> leagues;
 
-        public CustomAdapter(Context c, ArrayList<League> leagues) {
+        public CustomAdapter(Table c, ArrayList<League> leagues) {
             this.c = c;
             this.leagues = leagues;
         }
@@ -114,41 +118,23 @@ public class Table extends AppCompatActivity {
                 convertView = LayoutInflater.from(c).inflate(R.layout.league_model, parent, false);
             }
 
+            TextView Position = convertView.findViewById(R.id.Position);
+            TextView Team = convertView.findViewById(R.id.TeamName);
+            TextView Played = convertView.findViewById(R.id.Played);
+            TextView Goals = convertView.findViewById(R.id.Goals);
+            TextView Points = convertView.findViewById(R.id.Points);
+
+            final League s = (League) this.getItem(position);
+
+            Position.setText(s.getPosition());
+            Team.setText(s.getTeamName());
+            Played.setText(s.getPlayed());
+            Points.setText(s.getPoints());
+            Goals.setText(s.getGoals());
+
             return convertView;
         }
     }
-
-
-
-    /*class CustomAdapter extends BaseAdapter {
-        Context c;
-        ArrayList<League> leagues;
-
-        public  CustomAdapter (Context c, ArrayList<League> leagues) {
-            this.c = c;
-            this.leagues = leagues;
-        }
-
-        @Override
-        public int getCount() {
-            return leagues.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return leagues.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            return null;
-        }
-    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -162,6 +148,13 @@ public class Table extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         getSupportActionBar().setTitle("Division 1");
+
+        listView = (ListView) findViewById(R.id.listViewTxt);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        helper = new FirebaseHelper(databaseReference, this, listView);
+
+
 
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
